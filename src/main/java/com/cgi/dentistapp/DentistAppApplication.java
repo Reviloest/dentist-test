17ccefbce07d3c3dc92231e623e119aef62a5283
oliver <ollaid@ttu.ee>
2021-04-28 11:08:31 +0300

    Added more default registrations for better overview

--- src/main/java/com/cgi/dentistapp/DentistAppApplication.java
+++ src/main/java/com/cgi/dentistapp/DentistAppApplication.java
@@ -1,12 +1,15 @@
 package com.cgi.dentistapp;
 
 import com.cgi.dentistapp.entity.Dentist;
+import com.cgi.dentistapp.entity.DentistVisitEntity;
 import com.cgi.dentistapp.repository.DentistRepository;
+import com.cgi.dentistapp.repository.DentistVisitRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -18,11 +21,21 @@ public class DentistAppApplication {
     }
 
     @Bean
-    public CommandLineRunner init(DentistRepository repository) {
+    public CommandLineRunner init(DentistRepository repository, DentistVisitRepository dentistVisitRepository) {
         return args -> {
-            repository.save(new Dentist("Kuusk"));
-            repository.save(new Dentist("Kullerkupp"));
-            repository.save(new Dentist("Kuldsaar"));
+            Dentist dentist1 = new Dentist("Kuusk");
+            Dentist dentist2 = new Dentist("Kullerkupp");
+            Dentist dentist3 = new Dentist("Kuldsaar");
+            repository.save(dentist1);
+            repository.save(dentist2);
+            repository.save(dentist3);
+            Date currentDate = new Date();
+            dentistVisitRepository.save(new DentistVisitEntity(dentist2, currentDate, "08"));
+            dentistVisitRepository.save(new DentistVisitEntity(dentist1, currentDate, "08"));
+            dentistVisitRepository.save(new DentistVisitEntity(dentist1, currentDate, "11"));
+            dentistVisitRepository.save(new DentistVisitEntity(dentist3, currentDate, "08"));
+            dentistVisitRepository.save(new DentistVisitEntity(dentist2, currentDate, "09"));
+            dentistVisitRepository.save(new DentistVisitEntity(dentist1, currentDate, "14"));
         };
     }
 }
